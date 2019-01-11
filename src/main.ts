a09ffae279fbd5b96c9523d083f8150be6d42948
Erik P. Almaraz <erik@alkymizt.com>
2019-01-10 20:16:50 -0800

    adjust test feature

--- src/main.ts
+++ src/main.ts
@@ -11,28 +11,33 @@ interface Person {
 	lastName: string;
 }
 
-function greeter (person: Person) {
+function greeter (person: Person): Text {
 	return document.createTextNode(person.firstName + " " + person.lastName);
 }
 
 function post(): void {
-	let user = new Student("Hello", ", World! ", "Watsonville, CA");
-	let x = document.createElement('p');
-	let t = greeter(user);
-	x.appendChild(t);
-	document.body.appendChild(x);
+	let para = document.createElement('p');
+	para.setAttribute('id', 'world')
+	let node = greeter(new Student("Hello", ", World! ", "Watsonville, CA"));
+	para.appendChild(node);
+	document.body.appendChild(para);
+	setTimeout(cb => {
+		let child = document.getElementById('world');
+		child.parentNode.removeChild(child);
+	}, 4000);
+
 }
 
 function post2(): any {
 	let para = document.createElement('h2');
-	para.setAttribute('id','test');
+	para.setAttribute('id','test2');
 	let node = document.createTextNode("We are currently under construction...  In the meantime, checkout the ''More'' tab to see how I'm building this website:");
 	para.appendChild(node);
 	document.body.appendChild(para);
-	setTimeout(y => {
-		let child = document.getElementById('test');
+	setTimeout(cb => {
+		let child = document.getElementById('test2');
 		child.parentNode.removeChild(child);
-	}, 4000)
+	}, 4000);
 }
 
 function reload(): void {
