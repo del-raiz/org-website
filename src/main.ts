52d33a22759069ec101241ecb6b103a1075b4d97
Erik Almara <erik@alkymizt.com>
2019-12-14 08:07:41 -0800

    Fixed some breaks

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,46 @@
-import { isValid } from './lib/parser';
+//import { isValid } from './lib/parser';
 
 class Citizen {
-	fullName: string;
+    fullName: string;
 
-	constructor(public firstName: string, public lastName: string, public city: string) {
-		this.fullName = firstName + " " + lastName + " from " + city;
-	}
+    constructor(public firstName: string, public lastName: string, public city: string) {
+        this.fullName = firstName + " " + lastName + " from " + city;
+    }
 }
 
 interface Person {
-	firstName: string;
-	lastName: string;
+    firstName: string;
+    lastName: string;
 }
 
-function greeter (person: Person): string {
-	return person.firstName + " " + person.lastName;
+function greeter(person: Person): string {
+    return person.firstName + " " + person.lastName;
 }
 
 function activate(id: string): void {
-	let element = document.getElementById(id);
-	element.classList.add('active');
+    let element = document.getElementById(id);
+    element.classList.add('active');
 }
 
 function post(): any {
-	let para = document.createElement('h2');
-	para.setAttribute('id', 'world')
-	let student = new Citizen("Hello", ", World! ", "Watsonville, CA");
-	let string = greeter(student);
-	console.log(string);
-	let node = document.createTextNode(string);
-	para.appendChild(node);
-	document.body.appendChild(para);
-	setTimeout(cb => {
-		let child = document.getElementById('world');
-		child.parentNode.removeChild(child);
-	}, 4000);
-
-}
-
-function post2(): any {
-	let para = document.createElement('h2');
-	para.setAttribute('id','test2');
-	let node = document.createTextNode("We are currently under construction...  In the meantime, checkout the ''More'' tab to see how I'm building this website:");
-	para.appendChild(node);
-	document.body.appendChild(para);
-	setTimeout(cb => {
-		let child = document.getElementById('test2');
-		child.parentNode.removeChild(child);
-	}, 4000);
+    let para = document.createElement('h2');
+    para.setAttribute('id', 'world')
+    let student = new Citizen("Hello", ", World! ", "Watsonville, CA");
+    let string = greeter(student);
+    console.log(string);
+    let node = document.createTextNode(string);
+    para.appendChild(node);
+    document.body.appendChild(para);
+    setTimeout(cb => {
+        let child = document.getElementById('world');
+        child.parentNode.removeChild(child);
+    }, 4000);
 }
 
 function reload(): void {
-	location.reload();
+    location.reload();
 }
 
 function check(str: string) {
-
- 	return "The parenthese string is " + isValid(str) + "!"
- }
+    return "The parenthese string is " + isValid(str) + "!"
+}
