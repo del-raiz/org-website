821977f359995bf516b9bd0aa1e0309c50530c29
Erik P. Almaraz <erik@almarazlabs.com>
2022-07-02 00:56:58 -0700

    Clean-up code and minor bug fixes.

--- build-site.el
+++ build-site.el
@@ -23,11 +23,11 @@
 (package-install 'htmlize)
 
 ;; Load the publishing system
-(require 'org)
+;;(require 'org)
 (require 'ox-publish)
 
 ;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
+(setq org-html-validation-link t              ;; Don't not show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
