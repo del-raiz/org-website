f698a3dc8f98117af1b95b3439b589bd6f385ecc
Erik P. Almaraz <erik.almaraz@outlook.com>
2022-12-08 18:20:01 -0800

    First update in a while - update build and site content.

--- build-site.el
+++ build-site.el
@@ -6,34 +6,29 @@
 ;; License: MIT
 
 ;;; Code:
+;; #!/usr/bin/emacs -x
 
-;; Set the package installation directory so that packages aren't stored in the
-;; ~/.emacs.d/elpa path.
 (require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+ (setq package-user-dir (expand-file-name "./.packages"))
+ (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;; Initialize the package system
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+ (package-initialize)
+ (unless package-archive-contents
+   (package-refresh-contents))
 
-;; Install dependencies
-(package-install 'htmlize)
+ (package-install 'htmlize)
 
-;; Load the publishing system
 (require 'ox-publish)
 
 ;; Customize the HTML output
-(setq org-html-validation-link t               ;; Don't not show validation link
-      org-html-head-include-scripts nil        ;; Use our own scripts
+(setq org-html-validation-link t              ;; Don't not show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil) ;; Use our own styles
 
 (setq org-html-head (concat "<link rel=\"stylesheet\" "
                             "href=\"https://cdn.simplecss.org/simple.min.css\"/>"))
 
-;; Define the publishing project
 (setq org-publish-project-alist
       '(("org-website"
          :base-directory "./content"
@@ -42,7 +37,7 @@
          :recursive t
          :exclude "org-html-themes/.*"
          :publishing-function 'org-html-publish-to-html
-         :headline-levels 4
+         :headline-levels 4   ; default for now
          :with-aurthor t
          :with-creator t
          :with-toc t
@@ -50,8 +45,8 @@
          :time-stamp-file t
          :auto-preamble t)))
 
-
 (org-publish-all t)
+
 (message "Build complete!")
 
 
